be2ce1a7d2bd281edfe34c449a4f514493eeea14
Kriukovchankiu <kryukovchanskiu@gmail.com>
2023-05-16 16:17:09 +0300

    Added sorting of posts in the admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post
+@admin.register(Post)
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'status')
+    list_status = ('status','created','publish','author')
+    search_fields = ('title', 'body')   # поле поиска
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'  # ссылки для навигации по датам
+    ordering = ('status', 'publish')
