4173a84c8e89537ae9845a125e4dd04bb8612570
Kriukovchankiu <kryukovchanskiu@gmail.com>
2023-05-19 23:55:31 +0300

    Added article commenting system

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return  super().get_queryset().filter(status='published')
@@ -29,3 +30,17 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post,on_delete=models.CASCADE,related_name='comments')
+    name = models.CharField(max_length=30)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+    def __str__(self):
+        return 'Comment by {} on {}'.format(self.name, self.post)
